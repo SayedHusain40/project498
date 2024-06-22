0ca0e749bc3485986bc9483b39ccc6b17f2499d5
s.husain <s.husain_10@hotmail.com>
2024-06-22 20:23:17 +0300

    t

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,7 +5,7 @@
 @section('content')
     <div class="row row-card-no-pd">
         <div style="font-size: 20px">
-            Home Page
+            Home Pagec
         </div>
     </div>
 @endsection
