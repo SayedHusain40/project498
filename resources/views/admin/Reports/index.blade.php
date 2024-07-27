962e8f63c4c3d3325f5f4454de7652938dd27084
Elias-Kuw <eli.kuwaitan@gmail.com>
2024-07-27 19:59:02 +0300

    commit

--- resources/views/admin/Reports/index.blade.php
+++ resources/views/admin/Reports/index.blade.php
@@ -2,15 +2,25 @@
 
 @section('content')
     <h1>Generate Reports</h1>
+
     <form action="{{ route('admin.reports.generate') }}" method="POST">
         @csrf
-        <label for="report_type">Select Report Type:</label>
-        <select name="report_type" id="report_type">
-        <option value="users">Users Report</option>
-        <option value="departments">Departments Report</option>
-        <option value="colleges">Colleges Report</option>
-        <option value="courses">Courses Report</option>
+<div class="container mt-5">
+    <div class="mb-3">
+        <label for="report_type" class="form-label">Select Report Type:</label>
+        <select name="report_type" id="report_type" class="form-select">
+            <option value="users">Users Report</option>
+            <option value="departments">Departments Report</option>
+            <option value="colleges">Colleges Report</option>
+            <option value="courses">Courses Report</option>
         </select>
-        <button type="submit">Generate Report</button>
+    </div>
+</div>
+        <div class="mt-4">
+        <button type="submit" class="btn btn-primary w-100 py-2 px-4 rounded-lg shadow-md hover:bg-blue-700 transition duration-300">
+                                    Generrate Report
+        </button>
+        </div>
     </form>
+
 @endsection
