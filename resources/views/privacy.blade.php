709823988a40787bc0e76829ec572920b6817f4e
s.husain <s.husain_10@hotmail.com>
2024-11-26 16:50:10 +0300

    .

---
+++ resources/views/privacy.blade.php
@@ -0,0 +1,10 @@
+@extends('new_layouts.app')
+
+@section('content')
+    <div class="container mt-5">
+        <h1 class="display-4 text-center">Privacy Policy</h1>
+        <p class="lead">
+            content
+        </p>
+    </div>
+@endsection
---
+++ resources/views/terms.blade.php
@@ -0,0 +1,11 @@
+
+@extends('new_layouts.app')
+
+@section('content')
+    <div class="container mt-5">
+        <h1 class="display-4 text-center">Terms of Service</h1>
+        <p class="lead">
+            content
+        </p>
+    </div>
+@endsection
